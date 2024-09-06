dc9f7941f06147b90d7af6b4ea9bc279a748345c
applemcosare <applemcosare21@gmail.com>
2024-09-06 08:42:13 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Apple M. Cosare';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Creative UI Designer specializing in crafting intuitive and aesthetically pleasing user interfaces for web and mobile applications. Expert in translating user requirements into interactive, visually appealing designs.';
+export const ABOUT_TEXT_2 =
+  'Responsible for developing and maintaining user-centered designs that enhance user experience and satisfaction. Collaborate closely with cross-functional teams to ensure cohesive and functional design solutions.';
+export const ABOUT_TEXT_3 =
+  'Versatile designer proficient in both web and mobile design, capable of delivering complete user interface solutions from concept to implementation. Strong understanding of design principles, user psychology, and front-end technologies.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'UI Designer with 12+ years of experience in creating engaging and user-friendly interfaces for websites and applications. Extensive knowledge of design tools, front-end technologies, and user experience principles.';
+export const RESUME_TEXT_2 =
+  'UI Designer with 12+ years of experience crafting visually appealing and functional designs, ensuring a seamless user experience across digital platforms.';
